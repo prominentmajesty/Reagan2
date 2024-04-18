042b048edbcf49a8ada8aaaee05bfb53e4688b21
prominentmajesty <ugochukwuaugustine7@gmail.com>
2024-04-18 15:12:14 +0100

    Another commit

--- src/app/api/user/uploadresult/route.js
+++ src/app/api/user/uploadresult/route.js
@@ -63,7 +63,7 @@ export const POST = async(request, {params}) => {
                                    obj.grade = '';
                                    obj.remark = '';
                                    return obj;
-                                });
+                                }); 
                             } 
                             
                             result.primary.studentClass.activity1.Firstterm.overall = reformate_firstTest();
@@ -80,8 +80,7 @@ export const POST = async(request, {params}) => {
 
                             const firstTest = result.primary.studentClass.activity1.Firstterm.firstTest;
                             const secondTest = result.primary.studentClass.activity1.Firstterm.secondTest;
-                            const exam = result.primary.studentClass.activity1.Firstterm.exam;
-
+                            
                             var reformate_secondTest = function(){
                                 return secondTest.map(function(test, index) {
                                    const obj = {};
@@ -93,11 +92,13 @@ export const POST = async(request, {params}) => {
                                    const getFirstTest = parseInt(firstAssessment);
                                    const getSecondTest = parseInt(test.Value);
                                    //obj.exam = exam[index].Value;
+                                   obj.exam = '';
                                    obj.totalScore = getFirstTest + getSecondTest;
                                    const setAverage = Math.floor(obj.totalScore/2);
                                    obj.average = setAverage; 
-                                   const setRemark = ''
-                                   obj.remark = setRemark
+                                   obj.grade = '';
+                                   const setRemark = '';
+                                   obj.remark = ''
                                    return obj;
                                 });
                             }
@@ -106,9 +107,7 @@ export const POST = async(request, {params}) => {
 
                             //var remark = obj.average < 40 ? ' Failed ' : obj.average > 39 && obj.average < 46 ? 'Below Average' : obj.average > 45 && obj.average < 50 ? 'Fair' : obj.average > 49 && obj.average < 56 ? 'Average' : obj.average > 55 && obj.average < 61 ? 'Above Average' : obj.average > 61 && obj.average < 71 ? 'Good' : obj.average > 70 && obj.average < 81 ? 'Good Enough' : obj.average > 80 && obj.average < 91 ? 'Very Good' : obj.average > 90 && obj.average < 96 ? 'Excelent' : obj.average > 95 && obj.average < 101 ? 'Distinction' : 'Score more than hundred is not supported';
                             //var grade = obj.average < 40 ? 'F' : obj.average > 39 && obj.average < 46 ? 'E' : obj.average > 45 && obj.average < 50 ? 'D' : obj.average > 49 && obj.average < 56 ? 'C' : obj.average > 55 && obj.average < 61 ? 'C+' : obj.average > 60 && obj.average < 71 ? 'C++' : obj.average > 70 && obj.average < 81 ? 'B' : obj.average > 80 && obj.average < 91 ? 'B+' : obj.average > 90 && obj.average < 96 ? 'A': obj.average > 95 && obj.average < 101 ? 'A+' : 'Score more than hundred is not supported';
-                            const result = await Results.find({regNo : req.obj.details.regNo});
-                             
-                            console.log(JSON.stringify(result[0].primary.studentClass.activity1.Firstterm.secondTest, null, 2));
+                            
                              return new Response('Success..', { status: 200 })
                        
                             }else if(assesment === 'exam'){
@@ -116,10 +115,43 @@ export const POST = async(request, {params}) => {
                                 res.primary.studentClass.activity1.Firstterm.exam = arr;
                                 await res.save();
                                 
-                                const result = await Results.find({regNo : req.obj.details.regNo});
-                                
-                                console.log(JSON.stringify(result, null, 2));
-                                return new Response('Success..', { status: 200 })
+                                var result = await Results.findOne({regNo : req.obj.details.regNo});
+
+                            const firstTest = result.primary.studentClass.activity1.Firstterm.firstTest;
+                            const secondTest = result.primary.studentClass.activity1.Firstterm.secondTest;
+                            const exam = result.primary.studentClass.activity1.Firstterm.exam;
+                            var reformate_exam = function(){
+                                return exam.map(function(test, index) {
+                                   const obj = {};
+                                   obj.subject = test.Label;
+                                   obj.firstTest = firstTest.length === 0 ? 0 : firstTest[index].Value;
+                                   obj.secondTest = secondTest.length === 0 ? 0 : secondTest[index].Value;
+                                   obj.exam = test.Value;
+                                  
+                                   var firstAssessment = firstTest.length === 0 ? 0 : firstTest[index].Value;
+                                   var secondAssessment = secondTest.length === 0 ? 0 : secondTest[index].Value;
+                                   var getFirstTest = parseInt(firstAssessment);
+                                   var getSecondTest = parseInt(secondAssessment);
+                                   const getExam = parseInt(test.value);
+                                   
+            
+                                   obj.totalScore = getFirstTest + getSecondTest + getExam;
+                                   var setAverage = Math.floor(obj.totalScore/3);
+                                   obj.average = setAverage; 
+                                   var setGrade = obj.average < 40 ? 'F' : obj.average > 39 && obj.average < 46 ? 'E' : obj.average > 45 && obj.average < 50 ? 'D' : obj.average > 49 && obj.average < 56 ? 'C' : obj.average > 55 && obj.average < 61 ? 'C+' : obj.average > 60 && obj.average < 71 ? 'C++' : obj.average > 70 && obj.average < 81 ? 'B' : obj.average > 80 && obj.average < 91 ? 'B+' : obj.average > 90 && obj.average < 96 ? 'A': obj.average > 95 && obj.average < 101 ? 'A+' : 'Score more than hundred is not supported';
+                                   obj.grade = setGrade;
+                                   var setRemark = obj.average < 40 ? ' Failed ' : obj.average > 39 && obj.average < 46 ? 'Below Average' : obj.average > 45 && obj.average < 50 ? 'Fair' : obj.average > 49 && obj.average < 56 ? 'Average' : obj.average > 55 && obj.average < 61 ? 'Above Average' : obj.average > 61 && obj.average < 71 ? 'Good' : obj.average > 70 && obj.average < 81 ? 'Good Enough' : obj.average > 80 && obj.average < 91 ? 'Very Good' : obj.average > 90 && obj.average < 96 ? 'Excelent' : obj.average > 95 && obj.average < 101 ? 'Distinction' : 'Score more than hundred is not supported';
+                                   obj.remark = setRemark;
+                                   return obj;
+                                });
+                            }
+                            result.primary.studentClass.activity1.Firstterm.overall = reformate_exam();
+                            await result.save();
+
+                            //var remark = obj.average < 40 ? ' Failed ' : obj.average > 39 && obj.average < 46 ? 'Below Average' : obj.average > 45 && obj.average < 50 ? 'Fair' : obj.average > 49 && obj.average < 56 ? 'Average' : obj.average > 55 && obj.average < 61 ? 'Above Average' : obj.average > 61 && obj.average < 71 ? 'Good' : obj.average > 70 && obj.average < 81 ? 'Good Enough' : obj.average > 80 && obj.average < 91 ? 'Very Good' : obj.average > 90 && obj.average < 96 ? 'Excelent' : obj.average > 95 && obj.average < 101 ? 'Distinction' : 'Score more than hundred is not supported';
+                            //var grade = obj.average < 40 ? 'F' : obj.average > 39 && obj.average < 46 ? 'E' : obj.average > 45 && obj.average < 50 ? 'D' : obj.average > 49 && obj.average < 56 ? 'C' : obj.average > 55 && obj.average < 61 ? 'C+' : obj.average > 60 && obj.average < 71 ? 'C++' : obj.average > 70 && obj.average < 81 ? 'B' : obj.average > 80 && obj.average < 91 ? 'B+' : obj.average > 90 && obj.average < 96 ? 'A': obj.average > 95 && obj.average < 101 ? 'A+' : 'Score more than hundred is not supported';
+                            
+                             return new Response('Success..', { status: 200 })
 
                         }else {
                             console.log('No assesment');
